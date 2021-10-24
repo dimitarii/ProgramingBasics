80dbf9bcb8e51e5862b876819155199956693c88
Dimitar <84892557+dimitarii@users.noreply.github.com>
2021-10-24 14:31:24 -0400

    03 091021

---
+++ 03 091021 Conditional Statements - Lab/04.PasswordGuess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace softuni2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            if (text == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
---
+++ 03 091021 Conditional Statements - Lab/06.SpeedInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Day3SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Speed = double.Parse(Console.ReadLine());
+
+            if (Speed <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if (Speed <= 50)
+            {
+                Console.WriteLine("average");
+            }
+            else if (Speed <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (Speed <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else if (Speed > 1000)
+            {
+                Console.WriteLine("extremely fast");
+            }
+        }
+    }
+}
