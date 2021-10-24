e0ad03f5b5a3838e85a9b659f17dde75cf6fb619
Dimitar <84892557+dimitarii@users.noreply.github.com>
2021-10-24 11:59:17 -0400

    02 031021

---
+++ 02 031021 First Steps In Coding - Exercise/01.USDToBGN/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace softuni2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+
+            double bgn = usd * 1.79549;
+
+            Console.WriteLine(bgn);
+
+
+        }
+    }
+}
