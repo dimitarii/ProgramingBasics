f4cd00d402cce226633ed3c870c3b644c1c3d750
Dimitar <84892557+dimitarii@users.noreply.github.com>
2021-10-24 12:06:57 -0400

    02 031021

---
+++ 02 031021 First Steps In Coding - Exercise/02.RadiansToDegrees/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace softuni2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rasdians = double.Parse(Console.ReadLine());
+            double degrees = rasdians * 180 / Math.PI;
+            Console.WriteLine(degrees);
+        }
+    }
+}
