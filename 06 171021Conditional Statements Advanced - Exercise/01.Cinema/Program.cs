e51d7ebe78ff0f54f9d2140cf6de81969b5b0096
Dimitar <84892557+dimitarii@users.noreply.github.com>
2021-10-24 15:27:16 -0400

    06 171021

---
+++ 06 171021Conditional Statements Advanced - Exercise/01.Cinema/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace cinemaTickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+
+            double income = 0.0;
+
+            if (type == "Premiere")
+            {
+                income = rows * colums * 12.00;
+            }
+            else if (type == "Normal")
+            {
+                income = rows * colums * 7.50;
+            }
+            if (type == "Discount")
+            {
+                income = rows * colums * 5.00;
+            }
+            Console.WriteLine($"{income:F2} leva");
+        }
+    }
+}
